32ae324e85adf1bab453fb44ca18df7f927ca33f
unknown <zhuhuotui@gmail.com>
2014-10-28 17:12:07 +0800

    format to save as pdf

--- zReader/shanbay.js
+++ zReader/shanbay.js
@@ -1,5 +1,10 @@
+delete_node_by_class('span8 center');
 var article = document.getElementsByClassName('article-content')[0]
 article.addEventListener("DOMSubtreeModified", function(e) {
+  //end = document.getElementById('article-end');
+  //if (end != null) {
+  //  delete_node_by_id('article-end');
+  //}
   var paged = document.getElementsByClassName('paged')[0];
   if (paged) {
     paged.style.setProperty('display', 'none');
