f1fdd30ecefcddf4afdc06f8ac4bf546f62cfa3a
unknown <zhuhuotui@gmail.com>
2015-03-10 16:35:38 +0800

    zhihu

---
+++ zReader/zhihu.js
@@ -0,0 +1,7 @@
+delete_node_by_class('zu-top');
+delete_node_by_class('zh-answers-title');
+delete_node_by_class('more-awesome');
+delete_node_by_class('awesome-answer-list');
+delete_node_by_class('zu-main-sidebar');
+delete_node_by_class('zh-backtotop');
+delete_node_by_id('question-status');
