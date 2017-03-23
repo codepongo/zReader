800b684cfa0a382cf19e9c88998d244fe7ef03a9
unknown <zhuhuotui@gmail.com>
2017-03-23 17:40:35 +0800

    fix zhihu

--- zReader/zhihu.js
+++ zReader/zhihu.js
@@ -17,4 +17,34 @@ window.onload = function() {
   delete_node_by_class('zm-votebar goog-scrollfloater');
   delete_node_by_class('comment-app-holder');
   delete_node_by_class('FeedbackButton-button-3waL');
+  delete_node_by_class('QuestionMainDivider');
+  delete_node_by_class('List');
+  delete_node_by_class('QuestionMainAction');
 }
+
+
+delete_node_by_tag('header');
+delete_node_by_class('QuestionHeader-side');
+delete_node_by_class_when_event('QuestionHeader-side');
+delete_node_by_class_when_event('Sticky QuestionHeader-footer is-bottom');
+delete_node_by_class('QuestionHeader-footer');
+delete_node_by_class('Question-sideColumn');
+delete_node_by_class('CornerButtons');
+delete_node_by_class_when_event('QuestionMainDivider');
+delete_node_by_class_when_event('List');
+delete_node_by_class_when_event('QuestionMainAction');
+getElementsByClassName('QuestionHeader-main')[0].style.width='100%';
+getElementsByClassName('Question-mainColumn')[0].style.width='100%';
+//getElementsByClassName(
+getElementsByClassName('QuestionHeader')[0].style.borderBottom='0';
+getElementsByClassName('QuestionHeader')[0].style.boxShadow = '0 0 0 0';
+getElementsByClassName('Question-mainColumn')[0].style.width = '100%';
+getElementsByClassName('Card').forEach(function(e) {
+  console.log(e);
+  e.style.border='0px';
+  e.style.borderColor='black';
+  e.style.boxShadow = '0 0 0 0';
+});
+delete_node_by_class_when_event('ContentItem-actions Sticky is-bottom');
+delete_node_by_class_when_event('ContentItem-actions');
+getElementsByClassName('Button QuestionRichText-more Button--plain')[0].click();
